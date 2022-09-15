f059d1198ba01091c8c6b912f1c701529caa0eaf
MahDogan <113525199+MahDogan@users.noreply.github.com>
2022-09-15 14:03:13 +0200

    Version 2 mit 2 Aufrufparametern

--- seq/SeqProgramm.java
+++ seq/SeqProgramm.java
@@ -1,7 +1,9 @@
 public class SeqProgramm {
-    public static void main(String args[]) {
-        for (int i = 1; i <= 50; i++) {
-            System.out.println(i);
-        }
+    public static void main(String[] args) {
+        int param1 = Integer.parseInt(args[0]);
+        int param2 = Integer.parseInt(args[1]);				
+            for(int i = param1; i <= param2; i++) {
+                System.out.println(i);
+	    }
     }
-} 
+}
