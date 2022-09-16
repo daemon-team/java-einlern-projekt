1aae9888a28f88355a17840a3a348793701107b5
MahDogan <113525199+MahDogan@users.noreply.github.com>
2022-09-16 10:03:52 +0200

    Eine Sekunde Pause nur an einer Stelle

--- seq/SeqProgramm.java
+++ seq/SeqProgramm.java
@@ -1,7 +1,7 @@
 public class SeqProgramm {
     public static void main(String[] args) {
-        try {
-            if (args.length == 1) {
+        if (args.length == 1) {
+            try{
                 int param1= Integer.parseInt(args[0]);
                 for(int i= 1; i <= param1; i++) {
                     Thread.sleep(1000);
@@ -10,27 +10,25 @@ public class SeqProgramm {
                     } else {
                         System.err.println(i);
                     }
-                }          
-            }
-            if (args.length == 2) {
-                int param1= Integer.parseInt(args[0]);
-                int param2= Integer.parseInt(args[1]);
-                for (int i= param1; i <= param2; i++) {
-                    Thread.sleep(1000);
-                    System.out.println(i);
                 }
-            }	
-            if (args.length == 3) {
-                int param1= Integer.parseInt(args[0]);
-                int param2= Integer.parseInt(args[1]);
-                int param3= Integer.parseInt(args[2]);
-                for (int i= param1; i <= param3; i=i+param2) {
-                    Thread.sleep(1000);
-                    System.out.println(i);
-                }
+            } catch (InterruptedException e){
+                System.out.println(e);
+            }          
+        }
+        if (args.length == 2) {
+            int param1= Integer.parseInt(args[0]);
+            int param2= Integer.parseInt(args[1]);
+            for (int i= param1; i <= param2; i++) {
+                System.out.println(i);
             }
-        } catch (InterruptedException e) {
-            System.out.println(e);
+        }	
+        if (args.length == 3) {
+            int param1= Integer.parseInt(args[0]);
+            int param2= Integer.parseInt(args[1]);
+            int param3= Integer.parseInt(args[2]);
+            for (int i= param1; i <= param3; i=i+param2) {
+                System.out.println(i);
+            }
         }
     }
 }
