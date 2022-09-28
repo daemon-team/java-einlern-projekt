02650dd7aa78cbfa0118556df84554423745a371
MahDogan <113525199+MahDogan@users.noreply.github.com>
2022-09-28 09:46:20 +0200

    Startwert, Endwert und Schrittweite hinzugefügt.

--- src/main/java/SeqProgramm.java
+++ src/main/java/SeqProgramm.java
@@ -1,8 +1,12 @@
 public class SeqProgramm {
     public static void main(String[] args) {
-        if (args.length == 1) {
-            try{
+        try {
+            if (args.length == 1) {
                 int param1= Integer.parseInt(args[0]);
+                int start= 1;
+                int endwert= param1;
+                int schrittweite = 0;
+                schrittweite = schrittweite +1;
                 for(int i= 1; i <= param1; i++) {
                     Thread.sleep(1000);
                     if (i % 2 == 0) {
@@ -10,25 +14,34 @@ public class SeqProgramm {
                     } else {
                         System.err.println(i);
                     }
+                }          
+            }
+            if (args.length == 2) {
+                int param1= Integer.parseInt(args[0]);
+                int param2= Integer.parseInt(args[1]);
+                int start= param1;
+                int endwert=param2;
+                int schrittweite= 0;
+                schrittweite = schrittweite + 1;
+                for (int i= param1; i <= param2; i++) {
+                    Thread.sleep(1000);
+                    System.out.println(i);
                 }
-            } catch (InterruptedException e){
-                System.out.println(e);
-            }          
-        }
-        if (args.length == 2) {
-            int param1= Integer.parseInt(args[0]);
-            int param2= Integer.parseInt(args[1]);
-            for (int i= param1; i <= param2; i++) {
-                System.out.println(i);
+            }	
+            if (args.length == 3) {
+                int param1= Integer.parseInt(args[0]);
+                int param2= Integer.parseInt(args[1]);
+                int param3= Integer.parseInt(args[2]);
+                int start= param1;
+                int endwert=param3;
+                int schrittweite= param1+param2;
+                for (int i= param1; i <= param3; i=i+param2) {
+                    Thread.sleep(1000);
+                    System.out.println(i);
+                }
             }
-        }	
-        if (args.length == 3) {
-            int param1= Integer.parseInt(args[0]);
-            int param2= Integer.parseInt(args[1]);
-            int param3= Integer.parseInt(args[2]);
-            for (int i= param1; i <= param3; i=i+param2) {
-                System.out.println(i);
-            }
+        } catch (InterruptedException e) {
+            System.out.println(e);
         }
     }
 }
