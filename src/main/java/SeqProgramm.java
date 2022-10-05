9af30f500eac40ffc68d832356eeb3b487838df7
Mahmud Dogan <mdogan@meeting-laptop.lan>
2022-10-05 10:36:14 +0200

    Version mit nur einer For-Schleife

--- src/main/java/SeqProgramm.java
+++ src/main/java/SeqProgramm.java
@@ -1,34 +1,34 @@
 public class SeqProgramm {
     public static void main(String[] args) {
-        if (args.length == 1) {
-            try{
-                int param1= Integer.parseInt(args[0]);
-                for(int i= 1; i <= param1; i++) {
-                    Thread.sleep(1000);
-                    if (i % 2 == 0) {
-                        System.out.println(i);
-                    } else {
-                        System.err.println(i);
-                    }
+        try {
+            int Startwert= 0;
+            int Endwert= 0;
+            int Schrittweite= 1;
+            if (args.length == 1) {
+            	Startwert= 1;
+                Endwert= Integer.parseInt(args[0]);
+            }
+            if (args.length == 2) {
+                Startwert= Integer.parseInt(args[0]);
+                Endwert=  Integer.parseInt(args[1]);    
+            }	
+            if (args.length == 3) {
+                Startwert= Integer.parseInt(args[0]);
+                Schrittweite= Integer.parseInt(args[1]);
+                Endwert= Integer.parseInt(args[2]);
+            }
+            System.out.println("start: " + start + '\n' + "Schrittweite: " + schrittweite + '\n' + "end: " + end);
+            for(int i= Startwert; i <= Endwert; i= i + Schrittweite) {
+                Thread.sleep(1000);
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                } else {
+                    System.err.println(i);
                 }
-            } catch (InterruptedException e){
-                System.out.println(e);
             }          
-        }
-        if (args.length == 2) {
-            int param1= Integer.parseInt(args[0]);
-            int param2= Integer.parseInt(args[1]);
-            for (int i= param1; i <= param2; i++) {
-                System.out.println(i);
-            }
-        }	
-        if (args.length == 3) {
-            int param1= Integer.parseInt(args[0]);
-            int param2= Integer.parseInt(args[1]);
-            int param3= Integer.parseInt(args[2]);
-            for (int i= param1; i <= param3; i=i+param2) {
-                System.out.println(i);
-            }
+        } catch (InterruptedException e) {
+            System.out.println(e);
         }
     }
 }
+
