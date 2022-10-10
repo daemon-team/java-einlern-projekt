72e45fd66f9115b5836bf29af0338c238076230a
Mahmud Dogan <mdogan@meeting-laptop.lan>
2022-10-10 11:07:39 +0200

    Funktioniert nun mit Negativ Zahlen

--- src/main/java/SeqProgramm.java
+++ src/main/java/SeqProgramm.java
@@ -7,24 +7,40 @@ public class SeqProgramm {
             if (args.length == 1) {
             	Startwert= 1;
                 Endwert= Integer.parseInt(args[0]);
+                if(Endwert < 0){
+                    System.exit(0);
+                }
             }
             if (args.length == 2) {
                 Startwert= Integer.parseInt(args[0]);
-                Endwert=  Integer.parseInt(args[1]);    
+                Endwert=  Integer.parseInt(args[1]);
+                if(Startwert > Endwert){
+                    System.exit(0);
+                }    
             }	
             if (args.length == 3) {
                 Startwert= Integer.parseInt(args[0]);
                 Schrittweite= Integer.parseInt(args[1]);
                 Endwert= Integer.parseInt(args[2]);
-            }
-            for(int i= Startwert; i <= Endwert; i= i + Schrittweite) {
-                Thread.sleep(1000);
-                if (i % 2 == 0) {
-                    System.out.println(i);
-                } else {
-                    System.err.println(i);
+                if(Startwert < Endwert && Schrittweite < 0 || Startwert > Endwert && Schrittweite > 0){
+                    System.exit(0);
                 }
-            }          
+            }
+            if(Startwert < Endwert){
+                for(int i= Startwert; i <= Endwert; i= i + Schrittweite) {
+                    Thread.sleep(1000);
+                    if (i % 2 == 0 || -i % 2 == 0) {
+                        System.out.println(i);
+                    } else {
+                        System.err.println(i);
+                    }
+                }
+            }
+            if(Startwert > Endwert){
+                for(int i= Startwert; i >= Endwert; i = i+ Schrittweite){
+                    System.out.println(i);
+                }
+            }      
         } catch (InterruptedException e) {
             System.out.println(e);
         }
