3214d486d55767bb37b3261e3506879ddd45a49d
Mahmud Dogan <mdogan@meeting-laptop.lan>
2022-10-19 14:32:16 +0200

    Eine Schleife & Featureflags angepasst

--- src/main/java/SeqProgramm.java
+++ src/main/java/SeqProgramm.java
@@ -1,67 +1,49 @@
 public class SeqProgramm {
     public static void main(String[] args) {
         try {
-            boolean pause = Boolean.parseBoolean(args[0]);
-            boolean ausgabe = Boolean.parseBoolean(args[1]);
-            int Startwert= 0;
-            int Endwert= 0;
-            int Schrittweite= 1;
-            if (args.length == 3) {
-            	Startwert= 1;
-                Endwert= Integer.parseInt(args[2]);
-                if(Endwert < 0){
-                    System.exit(0);
-                }
+            boolean pause = true;
+            boolean ausgabe = true;
+            int startwert = 0;
+            int endwert = 0;
+            int schrittweite = 1;
+            int maxDurchgang = 0;
+            int jetztDurchgang = 1;
+            if (args.length == 1) {
+            	startwert= 1;
+                endwert= Integer.parseInt(args[0]);
             }
-            if (args.length == 4) {
-                Startwert= Integer.parseInt(args[2]);
-                Endwert=  Integer.parseInt(args[3]);
-                if(Startwert > Endwert){
-                    System.exit(0);
-                }     
+            if (args.length == 2) {
+                startwert = Integer.parseInt(args[0]);
+                endwert =  Integer.parseInt(args[1]); 
             }	
-            if (args.length == 5) {
-                Startwert= Integer.parseInt(args[2]);
-                Schrittweite= Integer.parseInt(args[3]);
-                Endwert= Integer.parseInt(args[4]);
-                if(Startwert < Endwert && Schrittweite < 0 || Startwert > Endwert && Schrittweite > 0){
-                    System.exit(0);
-                }
+            if (args.length == 3) {
+                startwert = Integer.parseInt(args[0]);
+                schrittweite = Integer.parseInt(args[1]);
+                endwert = Integer.parseInt(args[2]);
             }
-            if(Startwert < Endwert){
-                for(int i= Startwert; i <= Endwert; i= i + Schrittweite) {
-                    if(pause == true){
-                        Thread.sleep(1000);
-                    }
-                    if(ausgabe == true){
-                        if (i % 2 == 0) {
-                            System.out.println(i);
-                        } else {
-                            System.err.println(i);
-                        }
-                    }
-                    if(pause == false && ausgabe == false){
+            maxDurchgang = ((endwert - startwert) / schrittweite) + 1;
+            for (int i = startwert; jetztDurchgang <= maxDurchgang; jetztDurchgang++, i = i + schrittweite) {
+                if (pause == true) {
+                    Thread.sleep(1000);
+                    if (ausgabe == false) {
                         System.out.println(i);
                     }
                 }
-            if(Startwert > Endwert){
-                for(int i= Startwert; i >= Endwert; i = i+ Schrittweite){
-                    if(pause == true){
-                        Thread.sleep(1000);
-                    }
-                    if(ausgabe == true){
-                        if (i % 2 == 0) {
-                            System.out.println(i);
-                        } else {
-                            System.err.println(i);
-                        }
-                    }
-                    if(pause == false && ausgabe == false){
+                if (ausgabe == true) {
+                    if (i % 2 == 0) {
                         System.out.println(i);
+                    } else {
+                        System.err.println(i);
+                    }
                 }
-            }          
+                if (pause == false && ausgabe == false) {
+                    System.out.println(i);
+                }
+            }     
         } catch (InterruptedException e) {
             System.out.println(e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("fehlender Operand");
         }
     }
 }
